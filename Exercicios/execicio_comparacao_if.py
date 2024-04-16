621bd702ebff670e69536f584bade2b1d779163e
Renan <renan.victor325@outlook.com>
2024-04-16 19:57:21 -0300

    if-elif-else

---
+++ Exercicios/execicio_comparacao_if.py
@@ -0,0 +1,9 @@
+primeiro_valor = input('Digite o primeiro valor: ')
+segundo_valor = input('Digite segundo valor: ')
+
+
+
+if primeiro_valor > segundo_valor:
+    print(f' {primeiro_valor} é maior que {segundo_valor}')
+else:
+    print(f' {segundo_valor} é maior que {primeiro_valor}')
--- Exercicios/imc.py
+++ Exercicios/imc.py
@@ -6,14 +6,14 @@ peso = input('Digite seu Peso: ')
 peso = float(peso)
 imc = peso / (altura ** 2)
 
-print(nome, 'tem', altura, 'de altura,', 'pesa', peso, 'kg e seu imc é', imc)
+linha =f'{nome} tem {altura:.2f} de altura e pesa {peso} kg e seu imc é {imc:.2f}'
+print(linha) 
 
 if imc >=18.50 and imc <= 25:
     print('Seu peso IMC está normal')
-
 elif imc > 25 and imc <= 29.99:
     print('Pré-Obesidade')
-elif imc > 30 and imc <= 35:
+elif imc > 30 and imc <= 34.99:
     print('Obesidade Grau I')
 elif imc > 35 and imc <= 40 :
     print('Obesidade Grau II.')
